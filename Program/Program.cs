379a1774dae0a4772b606f7c19be2c7295ef5a8d
JVGouveia <gouveia.j.vitor@gmail.com>
2023-08-31 01:17:41 -0300

    Update Program.cs

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using Program.UI;
+
+class Principal
+{
+    static void Main()
+    {
+        try {
+            Console.Clear();
+            int choice;
+
+            do
+            {
+                Console.WriteLine("Loja de roupas GMV");
+                Console.WriteLine("1. Gerenciar Categorias");
+                Console.WriteLine("2. Gerenciar Produtos");
+                Console.WriteLine("3. Gerenciar Clientes");
+                Console.WriteLine("4. Gerenciar Vendas");
+                Console.WriteLine("5. Sair");
+                Console.Write("Escolha a operação: ");
+
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Clear();
+                            CategoriaUI.Run();
+                            Console.Clear();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            ProdutoUI.Run();
+                            Console.Clear();
+                            break;
+                        case 3:
+                            Console.Clear();
+                            ClienteUI.Run();
+                            Console.Clear();
+                            break;
+                        case 4:
+                            Console.Clear();
+                            VendaUI.Run();
+                            Console.Clear();
+                            break;
+                        case 5:
+                            Console.WriteLine("Saindo do programa.");
+                            break;
+                        default:
+                            Console.WriteLine("Opção inválida. Tente novamente.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                }
+
+            } while (choice != 5);
+        } catch (Exception e) {
+            Console.WriteLine("Erro: " + e.Message);
+        }
+    }
+}
